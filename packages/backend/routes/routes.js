9ad9b9b6d51733e5174f55242583c6ee99ea8147
Tomas Pedro <tpedro@frba.utn.edu.ar>
2025-10-06 20:46:33 -0300

    Agrego ruta de notificacion

--- packages/backend/routes/routes.js
+++ packages/backend/routes/routes.js
@@ -2,7 +2,8 @@ import pedidosRoutes from "./pedidosRoutes.js";
 import healthRoute from "./health.js";
 import productoRoutes from "./productoRoutes.js";
 import swaggerRoutes from "./swaggerRoutes.js";
+import notificacionesRoutes from "./notificacionesRoutes.js";
 
-const routes = [productoRoutes, pedidosRoutes, healthRoute, swaggerRoutes];
+const routes = [productoRoutes, pedidosRoutes, healthRoute, swaggerRoutes, notificacionesRoutes];
 
 export default routes;
